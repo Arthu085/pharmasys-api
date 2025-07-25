93ff7a098cc122ca0499809048c01424c180f5ca
Arthur Ghizi <arthurghizi22@gmail.com>
2025-07-25 14:22:26 -0300

    ajustes para hospedagem

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins =
+    process.env.NODE_ENV === 'production'
+      ? ['https://pharmasys-to4a.onrender.com'] // REMOTO
+      : ['http://localhost:5173']; // LOCAL
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
