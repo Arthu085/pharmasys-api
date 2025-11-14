0bb4f98e90bc6efe9ca181dbe55e8e18fd2d63f5
Arthu085 <arthurghizi22@gmail.com>
2025-11-14 14:04:16 -0300

    fix: removido execao inutilizada

--- src/modules/user/domain/exceptions/invalid-role-name.exception.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestException } from '@nestjs/common';
-
-export class InvalidRoleNameException extends BadRequestException {
-  constructor(message?: string) {
-    super(message || 'Nome de função inválido');
-  }
-}
