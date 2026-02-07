b543debfa772c5512ff3d219dce71bc37a8e280f
Arthu085 <arthurghizi22@gmail.com>
2026-02-07 17:24:43 -0300

    fix: adiciona nova verificacao para itens com dados repetidos

--- src/modules/item-dispensation/application/dtos/create-item-dispensation-request.dto.ts
+++ src/modules/item-dispensation/application/dtos/create-item-dispensation-request.dto.ts
@@ -3,6 +3,7 @@ import { IsArray, ValidateNested } from 'class-validator';
 
 import { ItemDispensationCreateDto } from './item-dispensation-create.dto';
 import { ItemDispensationItemCreateDto } from './item-dispensation-item-create.dto';
+import { UniqueCompositeArray } from 'src/shared/validation/unique-composite-array.decorator';
 
 export class CreateItemDispensationRequestDto {
   @ValidateNested()
@@ -11,6 +12,9 @@ export class CreateItemDispensationRequestDto {
 
   @IsArray()
   @ValidateNested({ each: true })
+  @UniqueCompositeArray(['item', 'batch'], {
+    message: 'Não é permitido repetir o mesmo item com o mesmo lote',
+  })
   @Type(() => ItemDispensationItemCreateDto)
   items: ItemDispensationItemCreateDto[];
 }
